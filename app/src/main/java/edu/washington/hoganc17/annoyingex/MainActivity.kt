5d0d545d1dde0d3289a5e131596320afa098922a
Chris Hogan <chrishogan17@outlook.com>
2020-05-19 16:23:47 -0700

    Initial project setup

---
+++ app/src/main/java/edu/washington/hoganc17/annoyingex/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.washington.hoganc17.annoyingex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
