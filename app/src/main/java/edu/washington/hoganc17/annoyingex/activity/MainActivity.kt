9ff4d5e76729883fc1a1b6a8f7f6c0460a6480ec
Chris Hogan <chrishogan17@outlook.com>
2020-05-20 17:03:25 -0700

    Add check to see if messages have already been fetched so that progress bar can be disabled

--- app/src/main/java/edu/washington/hoganc17/annoyingex/activity/MainActivity.kt
+++ app/src/main/java/edu/washington/hoganc17/annoyingex/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.washington.hoganc17.annoyingex.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import edu.washington.hoganc17.annoyingex.AnnoyingExApp
@@ -12,9 +13,21 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity(),
     MessagesFetchListener {
 
+    private var hasFetched = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        savedInstanceState?.let {
+            val prevFetchedStatus = savedInstanceState.getBoolean(OUT_HAS_FETCHED)
+            hasFetched = prevFetchedStatus
+        }
+
+        if (hasFetched) {
+            progressSpinner.visibility = View.GONE
+            tvLoading.visibility = View.GONE
+        }
 
         val notificationManager = (application as AnnoyingExApp).annoyingExNotificationManager
         notificationManager.messagesFetchListener = this
@@ -31,6 +44,7 @@ class MainActivity : AppCompatActivity(),
     }
 
     override fun onMessagesFetched(messages: List<String>) {
+        hasFetched = true
         progressSpinner.visibility = View.GONE
         tvLoading.visibility = View.GONE
         btnStartNotifications.visibility = View.VISIBLE
@@ -41,5 +55,14 @@ class MainActivity : AppCompatActivity(),
         Toast.makeText(this, "Error retrieving messages", Toast.LENGTH_SHORT).show()
         tvLoading.text = getString(R.string.errorRetrievingMessages)
         progressSpinner.visibility = View.GONE
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putBoolean(OUT_HAS_FETCHED, hasFetched)
+    }
+
+    companion object {
+        const val OUT_HAS_FETCHED = "OUT_HAS_FETCHED"
     }
 }
